02f395c4b838fc3838c2be09f285f9207b3fc232
Na <na@Mac.local>
2023-07-11 11:07:28 +0900

    indexと新規作成画面の追加

---
+++ db/migrate/20230710124028_create_schedules.rb
@@ -0,0 +1,14 @@
+class CreateSchedules < ActiveRecord::Migration[6.1]
+  def change
+    create_table :schedules do |t|
+      t.string :title
+      t.date :start_date
+      t.date :end_date
+      t.boolean :all_day
+      t.datetime :update_time
+      t.string :memo
+
+      t.timestamps
+    end
+  end
+end
